8905b52895cf08508de3fbba37d81c8500e7e517
JN159413 <student@woshizhengyifei-wozuishuai.local>
2016-06-01 11:26:49 +0800

    JN
    4646546

---
+++ ForCircie  苹果/ForCircie/main.m
@@ -0,0 +1,95 @@
+//
+//  main.m
+//  ForCircie
+//
+//  Created by MAC on 15/12/15.
+//  Copyright © 2015年 姜. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+//        NSLog(@"Hello, World!");
+
+//        if (<#condition#>) {
+//            <#statements#>
+//        }else if
+//            
+        
+        //
+//    
+//        for (int i = 0 ; i < 1; i++) {
+//            
+//            
+//            printf("@\n@@\n@@@\n@@@@\n@@@@@\n@@@@@@\n@@@@@@@\n@@@@@@@@\n@@@@@@@@@\n");
+//        }
+//    
+      
+        
+       //奇数偶数
+//        
+//        for  (int  i = 0; i <= 100; i ++) {
+//           
+//           
+//            
+//            if ( i%2>0 ) {
+//                NSLog(@"%d是奇数",i );
+//            }else {
+//            
+//                NSLog(@"%d是偶数",i );
+//            }
+//        }
+    
+    
+        
+//        
+//        if (<#condition#>) {
+//            <#statements#>
+//        }else if (<#expression#>){
+//        
+//        }else{
+//        }
+        
+        
+//        
+//        for (<#initialization#>; <#condition#>; <#increment#>) {
+//            if (<#condition#>) {
+//                <#statements#>
+//            }else if (<#expression#>)
+//        }
+        
+      
+        
+        for (int i = 1; i<10; i++) {
+            for (int a =1; a<=i ; a++) {
+                printf("");
+            }
+            printf("\n");
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    }
+    return 0;
+}
